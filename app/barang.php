53fc05036fcb27256756bcc085f79c375831371e
Atmasari <atmasari_kusuma_28rpl@student.smktelkom-mlg.schh.id>
2020-08-15 19:24:18 +0700

    penambahan komentar pada file barang.php

--- app/barang.php
+++ app/barang.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class barang extends Model
 {
     protected $table = 'barang'; // nama tabel nya salah
+    // coba ini saya update ya nak, saya tambahin komentar seperti ini, berarti kan
+    // berubah isi file nya, ketika di save, muncul di sebelah kanan.
+    // iya pak, laluu
+    // lalu stage all dan lakukan commit, setalah kommit berhasil, lakukan push
     public $timestamps = false;
 
     protected $fillable = ['kode_barang','nama_barang','harga','id_customer'];
